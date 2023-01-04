60c737ddf537ef40be2508e7441b6bde6b180310
Divish28 <divish.karthik@aspiresys.com>
2023-01-04 17:47:10 +0530

    Update

--- src/components/Mens.js
+++ src/components/Mens.js
@@ -3,13 +3,13 @@ import Navbar from './Navbar'
 import { useNavigate } from 'react-router-dom'
 
 function Mens() {
-  const Navigation=useNavigate()
+  const navigation=useNavigate()
   return (
 
     <div>
             {/* <Navbar/> */}
         <div>This is mens page</div>
-        <button onClick={()=>Navigation('/Order')}>Place order</button>
+        <button onClick={()=>navigation('/Order')}>Place order</button>
     </div>
   )
 }
